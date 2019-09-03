25908969ae2bf517c0d894109238c76a7973460a
Salcover <simonalcover@gmail.com>
2019-09-03 19:30:55 +0800

    translations update

--- src/Translations/en_EN.interface.php
+++ src/Translations/en_EN.interface.php
@@ -2,9 +2,10 @@
     return [
 		'tr_meliscodeexamplesilex_title' => 'Silex Tool Demo',
 		'tr_meliscodeexamplesilex_desc' => 'Silex Tool Demo',
-		'tr_meliscodeexamplesilex_album_id' => 'ID',
-		'tr_meliscodeexamplesilex_album_title' => 'Title',
-		'tr_meliscodeexamplesilex_album_date_added' => 'Date Added',
+        'tr_meliscodeexamplesilex_album_id' => 'ID',
+        'tr_meliscodeexamplesilex_album_title' => 'Title',
+        'tr_meliscodeexamplesilex_album_date' => 'Date',
+        'tr_meliscodeexamplesilex_album_songs' => 'Songs',
 		'tr_meliscodeexamplesilex_tool_title' => 'A tool embryo made in Silex',
 		'tr_meliscodeexamplesilex_tool_desc' => 'This demo tool has for goal to show how you can use the framework Silex inside Melis Platform, so you\'re not locked on ZF.',
 		'tr_meliscodeexamplesilex_tool_db_connection_desc' => 'The table below shows the data fetched from the database using Silex.',
