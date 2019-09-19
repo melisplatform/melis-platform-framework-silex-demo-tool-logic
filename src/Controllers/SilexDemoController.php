bb34675b062402cf302c4ff82125bf1ada85806a
Salcover <simonalcover@gmail.com>
2019-09-19 19:10:09 +0800

    silex templating plugin implementation

--- src/Controllers/SilexDemoController.php
+++ src/Controllers/SilexDemoController.php
@@ -9,6 +9,7 @@ class SilexDemoController implements ControllerProviderInterface {
     public function connect(Application $app) {
         $factory=$app['controllers_factory'];
         $factory->get('/silex-list','MelisPlatformFrameworkSilexDemoToolLogic\Controllers\SilexDemoController::silexDemo');
+        $factory->get('/silex-plugin','MelisPlatformFrameworkSilexDemoToolLogic\Controllers\SilexDemoController::silexPlugin');
         return $factory;
     }
 
@@ -22,5 +23,13 @@ class SilexDemoController implements ControllerProviderInterface {
         $albums = $app['dbs']['melis']->fetchAll($sql);
 
         return $app['twig']->render('demo.template.html.twig',array("albums" => $albums,"news"=>$news));
+    }
+    public function silexPlugin(Application $app) {
+
+        #using Melis Database;
+        $sql = "SELECT * FROM album ";
+        $albums = $app['dbs']['melis']->fetchAll($sql);
+
+        return $app['twig']->render('plugin.template.html.twig',array("albums" => $albums));
     }
 }
