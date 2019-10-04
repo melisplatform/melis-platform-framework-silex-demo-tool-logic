c670059822aff4595fdf392238727821e50b17a4
Salcover <simonalcover@gmail.com>
2019-10-04 11:46:02 +0800

    renamed album db name

--- src/Controllers/SilexDemoController.php
+++ src/Controllers/SilexDemoController.php
@@ -19,7 +19,7 @@ class SilexDemoController implements ControllerProviderInterface {
         $langs = $langSvc->getAvailableLanguages();
 
         #using Melis Database;
-        $sql = "SELECT * FROM album ";
+        $sql = "SELECT * FROM melis_demo_album ";
         $albums = $app['dbs']['melis']->fetchAll($sql);
 
         return $app['twig']->render('demo.template.html.twig',array("albums" => $albums,"langs"=>$langs));
@@ -27,7 +27,7 @@ class SilexDemoController implements ControllerProviderInterface {
     public function silexPlugin(Application $app) {
 
         #using Melis Database;
-        $sql = "SELECT * FROM album ";
+        $sql = "SELECT * FROM melis_demo_album ";
         $albums = $app['dbs']['melis']->fetchAll($sql);
 
         return $app['twig']->render('plugin.template.html.twig',array("albums" => $albums));
