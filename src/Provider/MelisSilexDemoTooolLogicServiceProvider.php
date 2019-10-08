06f9a6354794107ab67ea7c752520100ebc26977
Salcover <simonalcover@gmail.com>
2019-10-08 16:56:21 +0800

    update route

--- src/Provider/MelisSilexDemoTooolLogicServiceProvider.php
+++ src/Provider/MelisSilexDemoTooolLogicServiceProvider.php
@@ -53,7 +53,7 @@ class MelisSilexDemoTooolLogicServiceProvider implements BootableProviderInterfa
         });
 
         #Silex routing DEMO configuration using a Silex Controller provider;
-        $app->mount('/', new SilexDemoController());
+        $app->mount('melis/', new SilexDemoController());
 
 
         /**
