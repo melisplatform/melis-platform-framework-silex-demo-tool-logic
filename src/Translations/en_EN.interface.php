4f9ac6586cc96e450dd0d0d0d36a2d912f7bb056
Salcover <simonalcover@gmail.com>
2019-10-08 18:48:22 +0800

    hard code the texts in templating plugin to avoid translations

--- src/Translations/en_EN.interface.php
+++ src/Translations/en_EN.interface.php
@@ -13,8 +13,4 @@
 		'tr_meliscodeexamplesilex_tool_desc' => 'This demo tool has for goal to show how you can use the framework Silex inside Melis Platform, so you\'re not locked on ZF.',
 		'tr_meliscodeexamplesilex_tool_db_connection_desc' => 'The table below shows the data fetched from the database using Silex.',
 		'tr_meliscodeexamplesilex_tool_melis_services_desc' => 'The table below shows the data fetched from Melis Platform Services using Silex.',
-        'tr_meliscodeexamplesilex_plugin_title' => 'A tool embryo made in Silex',
-        'tr_meliscodeexamplesilex_plugin_desc' => 'This demo tool has for goal to show how you can use the framework Silex inside Melis Platform, so you\'re not locked on ZF.',
-        'tr_meliscodeexamplesilex_plugin_db_connection_desc' => 'The table below shows the data fetched from the database using Silex.',
-        'tr_meliscodeexamplesilex_plugin_melis_services_desc' => 'The table below shows the data fetched from Melis Platform Services using Silex.',
     ];
--- src/Translations/fr_FR.interface.php
+++ src/Translations/fr_FR.interface.php
@@ -13,8 +13,4 @@
         'tr_meliscodeexamplesilex_tool_desc' => 'Ce outil de demo a pour but de montrer comment utiliser le framework Silex à l\'intérieur de Melis Platform, et ne pas être bloqué en ZF.',
         'tr_meliscodeexamplesilex_tool_db_connection_desc' => 'Le tableau ci-dessous affiche les données extraites de la base de données en utilisant Silex.',
         'tr_meliscodeexamplesilex_tool_melis_services_desc' => 'Le tableau ci-dessous affiche les données extraites des services de Melis Platform en utilisant Silex.',
-        'tr_meliscodeexamplesilex_plugin_title' => 'A tool embryo made in Silex',
-        'tr_meliscodeexamplesilex_plugin_desc' => 'This demo tool has for goal to show how you can use the framework Silex inside Melis Platform, so you\'re not locked on ZF.',
-        'tr_meliscodeexamplesilex_plugin_db_connection_desc' => 'The table below shows the data fetched from the database using Silex.',
-        'tr_meliscodeexamplesilex_plugin_melis_services_desc' => 'The table below shows the data fetched from Melis Platform Services using Silex.',
     ];
